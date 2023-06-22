7f29d037495859cf456d4c22f9baa90e3915e363
cristino4 <117871733+cristino4@users.noreply.github.com>
2023-06-21 19:43:36 -0700

    updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -9,7 +9,7 @@ const Resume = () => {
         <div className="flex flex-col items-center my-10 mx-auto h-screen">
             <p className="text-white text-center">Click below to download my resume in PDF format</p>
             <div className="my-10">
-                <a href="./assets/20230614_Cristino_Castro_Resume_J.pdf" target="_blank" rel="noopener noreferrer"  className="text-2xl font-medium font-sans text-white border rounded-full p-4">Download Resume</a>
+                <a href="./assets/20230621_Cristino_Castro_Resume_K.pdf" target="_blank" rel="noopener noreferrer"  className="text-2xl font-medium font-sans text-white border rounded-full p-4">Download Resume</a>
             </div>
         </div>
     );
