f1f418af29f0e93af7c05ac93cb72186e5f9247f
cristino4 <117871733+cristino4@users.noreply.github.com>
2023-06-27 13:39:54 -0700

    changed resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -9,7 +9,7 @@ const Resume = () => {
         <div className="flex flex-col items-center my-10 mx-auto h-screen">
             <p className="text-white text-center">Click below to download my resume in PDF format</p>
             <div className="my-10">
-                <a href="./assets/images/20230623_Cristino_Castro_Resume_J.pdf" target="_blank" rel="noopener noreferrer"  className="text-2xl font-medium font-sans text-white border rounded-full p-4">Download Resume</a>
+                <a href="./assets/images/20230627_Cristino_Castro_Resume_L.pdf" target="_blank" rel="noopener noreferrer"  className="text-2xl font-medium font-sans text-white border rounded-full p-4">Download Resume</a>
             </div>
         </div>
     );
