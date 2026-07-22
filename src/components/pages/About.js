ad8c8ecf021d9c83a057b625ec8fd9126d337ae1
cristino4 <117871733+cristino4@users.noreply.github.com>
2026-07-22 14:37:10 -0700

    updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -13,20 +13,20 @@ const About = ({handlePageChange}) => {
                 <div className="text-white">
                     <p className="font-sans text-4xl font-bold text-center">Welcome!</p><br></br><br></br>
                     <p className="font-sans text-3xl font-bold text-center">I'm Cristino Castro</p>
-                    <p className="font-sans py-6">I'm an electrical engineer and software developer from Anaheim, California. <br></br><br></br>
+                    <p className="font-sans py-6">I'm an electrical engineer and software developer from Hesperia, California. <br></br><br></br>
                     I was raised in a small family with my only sibling and sister Caroline (seen with me above). 
                     Since I was a kid, my sister and I have been fascinated by science and engineering. I remember 
-                    binge watching Bill Nye (the science guy, remember?) and PBS NOVA shows on TV or cassette. In school I was thrilled at 
+                    binge watching Bill Nye (the science guy) and PBS NOVA shows on TV or cassette. In school I was thrilled at 
                     new science experiments in chemistry class and played guitar in a rock band with my chemistry teacher.  
                     <br></br><br></br>
                     During college, my hunger for knowledge led me to various school project experiences, scholarships that helped me become who I am today!
                     
-                    In the 8 years after college graduation, i've worked as a college robotics instructor, science tutor,
-                    Audio Visual (AV) electronics test engineer and later senior project engineer at Broadata Communications, and as an owner of a small cellphone repair shop. 
+                    After college graduation, i've worked as a college robotics instructor, science tutor, as an owner/operator of a small cellphone shop,
+                    vehicle crash test data aquisition engineer, and an Audio Visual (AV) electronics senior test engineer. 
                     <br></br><br></br>
-                    Today, I add web development to my many skills and am currently looking for a full-time position as a software or electronics engineer.
+                    I am currently looking for a full-time position as a software or electronics engineer.
                     You may download my resume <a href='Resume' onClick = {() => handlePageChange('Resume')} className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600' target="_blank" rel="noopener noreferrer" >here</a> 
-                    {} or look at my software development portfolio {}
+                    {} or look at my software portfolio {}
                     <a href='Portfolio' onClick = {() => handlePageChange('Portfolio')} className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600' target="_blank" rel="noopener noreferrer" >here</a> for more info.
                     <br></br><br></br>
                     Gracias!
